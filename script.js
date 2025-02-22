a43373b0d5c3a4e83ec288bd51bd69d6829ffa35
Alex Leite <j.alex.l.leite03@gmail.com>
2025-02-22 12:51:29 -0300

    v1.01 portifolio

--- script.js
+++ script.js
@@ -24,68 +24,68 @@ insta.addEventListener('mouseout', sair5);
 git.addEventListener('mouseenter', entrar6);
 git.addEventListener('mouseout', sair6);
 
-function entrar(){
-    ini.style.background = 'rgba(230, 207, 2, 0.9)'    
+function entrar() {
+    ini.style.background = 'rgba(230, 207, 2, 0.9)'
 }
 
-function sair(){
+function sair() {
     ini.style.background = 'rgba(230, 207, 2, 0)'
 }
 
-function entrar2(){
-    sob.style.background = 'rgba(230, 207, 2, 0.9)'  
+function entrar2() {
+    sob.style.background = 'rgba(230, 207, 2, 0.9)'
 }
 
-function sair2(){
+function sair2() {
     sob.style.background = 'rgba(230, 207, 2, 0)'
 }
 
-function entrar3(){
-    tec.style.background = 'rgba(230, 207, 2, 0.9)'   
+function entrar3() {
+    tec.style.background = 'rgba(230, 207, 2, 0.9)'
 }
 
-function sair3(){
+function sair3() {
     tec.style.background = 'rgba(230, 207, 2, 0)'
 }
 
-function entrar4(){
-    con.style.background = 'rgba(230, 207, 2, 0.9)'   
+function entrar4() {
+    con.style.background = 'rgba(230, 207, 2, 0.9)'
 }
 
-function sair4(){
+function sair4() {
     con.style.background = 'rgba(230, 207, 2, 0)'
 }
 
-function entrar5(){
-    insta.style.background = 'rgba(230, 207, 2, 0.2)'    
+function entrar5() {
+    insta.style.background = 'rgba(230, 207, 2, 0.2)'
 }
 
-function sair5(){
+function sair5() {
     insta.style.background = 'rgba(230, 207, 2, 1)'
 }
 
-function entrar6(){
-    git.style.background = 'rgba(230, 207, 2, 0.2)'    
+function entrar6() {
+    git.style.background = 'rgba(230, 207, 2, 0.2)'
 }
 
-function sair6(){
+function sair6() {
     git.style.background = 'rgba(230, 207, 2, 1)'
 }
 
 let cont = 0;
- function animacao(){
-    
-    if (cont < texto.length){
+function animacao() {
+
+    if (cont < texto.length) {
         document.getElementById('animacao').innerHTML += texto.charAt(cont);
         cont++;
         setTimeout(animacao, 60);
-    }else{
+    } else {
         setTimeout(() => {
             document.getElementById('animacao').innerHTML = "";
             cont = 0
             animacao();
         }, 500);
     }
- };
- 
- animacao();
+};
+
+animacao();
