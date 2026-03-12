a2f4d399d57aa446e661d0baf5fd2b3e66cb9c38
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-12 13:46:58 -0300

    update cap01/missao01/script.js

--- cap01/missao01/script.js
+++ cap01/missao01/script.js
@@ -1,12 +1,12 @@
 
 function proximo(){
-	document.getElementById("descricao").style.display = "none";
-	document.getElementById("texto").style.display = "block";
+	document.getElementById("texto1").style.display = "none";
+	document.getElementById("texto2").style.display = "block";
 }
 
 function voltar(){
-	document.getElementById("descricao").style.display = "block";
-	document.getElementById("texto").style.display = "none";
+	document.getElementById("texto1").style.display = "block";
+	document.getElementById("texto2").style.display = "none";
 	
 }
 
