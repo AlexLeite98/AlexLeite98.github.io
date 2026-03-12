4e395e94e18f8ddb0c8364ffc082dbdce8c8ea1a
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-12 12:52:57 -0300

    update cap01/missao01/script.js

--- cap01/missao01/script.js
+++ cap01/missao01/script.js
@@ -5,6 +5,9 @@ function tocar(){
 	document.getElementById("texto").style.display = "block";
 }
 
-
+function voltar(){
+	document.getElementById("descricao").style.display = "none";
+	document.getElementById("texto").style.display = "block";
+}
 
 
