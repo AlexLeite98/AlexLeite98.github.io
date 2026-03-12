be8be46acfb704bd8d1331746820bb765ec46324
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-12 11:18:05 -0300

    update cap01/missao01/script.js

--- cap01/missao01/script.js
+++ cap01/missao01/script.js
@@ -1,6 +1,7 @@
 
 function tocar(){
 	document.getElementById("musica").play();
+	document.getElementById("descricao").style.display = "none";
 	document.getElementById("texto").style.display = "block";
 }
 
