5fc0b1b6a1b33a97204dabbae75c0b84a5f652d4
Alex Leite <j.alex.l.leite03@gmail.com>
2026-03-15 14:32:44 -0300

    update cap02/script.js

---
+++ cap02/script.js
@@ -0,0 +1,37 @@
+function missao1(){
+	document.getElementById("texto1").style.display = "none";
+	document.getElementById("texto2").style.display = "block";
+}
+
+function missao2(){
+	document.getElementById("texto2").style.display = "none";
+	document.getElementById("texto3").style.display = "block";
+}
+
+function missao3(){
+	document.getElementById("texto3").style.display = "none";
+	document.getElementById("texto4").style.display = "block";
+}
+
+function missao4(){
+	document.getElementById("voltar").style.display = "none";
+	document.getElementById("texto4").style.display = "none";
+	document.getElementById("texto5").style.display = "block";
+	document.getElementById("fim").style.display = "block";
+}
+
+function fim(){
+	document.getElementById("fim").style.display = "none";
+	document.getElementById("texto5").style.display = "none";
+	document.getElementById("texto6").style.display = "block";
+	document.getElementById("cap2").style.display = "block";
+}
+
+function voltar(){
+	document.getElementById("texto1").style.display = "block";
+	document.getElementById("texto2").style.display = "none";
+	document.getElementById("texto3").style.display = "none";
+	document.getElementById("texto4").style.display = "none";
+	document.getElementById("texto5").style.display = "none";
+	
+}
